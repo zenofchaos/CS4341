d67d85661f5c5a3cb90b554ec6ef73d794572ee3
Kevin Ouellette <zenofchaos@comcast.net>
2017-01-25 16:53:19 -0500

    Node class framework

--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 import argparse
 import sys
 
+class Node:
+    
+    def __init__(self,p,x,y,c):
+        self.parent = p
+        self.xPos = x
+        self.yPos = y
+        self.cost = c
+    
+#----------------------------------------------------------------------------
 # Create the argument parser.
 parser = argparse.ArgumentParser(description="Read in a map and run A* on it.")
 
@@ -15,9 +24,23 @@ args = parser.parse_args()
 with open(args.map, 'r') as f:
     arr = [line.strip('\n').split('\t') for line in f]
 
+#DONT FORGET: arrays are -> arr[y][x]
 for i in range(0,len(arr)):
     for j in range(0,len(arr[i])):
-        arr[i][j] = (arr[i][j])
+        if (arr[i][j] == 'S'):
+            startNode = Node(None,j,i,1)
+        elif (arr[i][j] == 'G'):
+            goalNode = Node(None,j,i,1)
+        else:
+            continue
 
 for i in range(0, len(arr)):
 	print (arr[i])
+
+print (goalNode.parent,goalNode.xPos,goalNode.yPos,goalNode.cost)
+#----------------------------------------------------------------------------
+
+#init the open list
+
+#while (len.openList != 0)
+    
